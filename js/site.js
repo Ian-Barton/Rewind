7397852b2d0c1cbecf2ee3b4394bf6fe27fc950c
Ian-Barton <bobbarbid.1@gmail.com>
2022-08-17 09:38:12 -0500

    Done
    Did the project. A description as to what it does is on the Code page.

--- js/site.js
+++ js/site.js
@@ -1,3 +1,24 @@
-function HelloWorld() {
-    alert("Hello World");
+//Get the String input
+//Controller function
+function getValues() {
+    document.getElementById("alert").classList.add("invisible");
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+    displayReverse(revString);
+}
+//Reverse the String
+//Logic Function
+function reverseString(userString) {
+    let revString = [];
+
+    for (let i = userString.length - 1; i >=0; i--) {
+        revString += userString[i];
+    }
+    return revString;
+}
+//Display the message
+//View Function
+function displayReverse(revString) {
+    document.getElementById("msg").innerHTML = `You String reversed is: ${revString}`;
+    document.getElementById("alert").classList.remove("invisible");
 }
